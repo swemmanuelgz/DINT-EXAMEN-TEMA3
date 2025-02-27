57d6fdbebbbe16647f402ecab0130c555d380535
damdu217 <zundercoc@gmail.com>
2025-02-27 18:38:38 +0100

    acabando

--- src/main/java/com/example/demo1/UsuarioDAO.java
+++ src/main/java/com/example/demo1/UsuarioDAO.java
@@ -4,6 +4,7 @@ import com.example.demo1.modelo.Modelo;
 import java.util.List;
 
 public interface UsuarioDAO {
+    //no sé si está bien pero es la manera que suelo hacerlo
     void insertarDatos(Modelo usuario);
     List<Modelo> selectDatos();
 }
--- src/main/java/com/example/demo1/pruebasReports/ReportGenerating.java
+++ src/main/java/com/example/demo1/pruebasReports/ReportGenerating.java
@@ -9,6 +9,7 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.UnitValue;
+import javafx.scene.control.Alert;
 
 import java.io.FileOutputStream;
 import java.sql.Connection;
@@ -25,35 +26,37 @@ public class ReportGenerating {
     public void generateReport(Connection conn) {
         try {
             // Crea un documento PDF y un PdfDocument asociado
-            PdfWriter writer = new PdfWriter(new FileOutputStream("REPORTS/reporte_users.pdf"));
+            PdfWriter writer = new PdfWriter(new FileOutputStream("REPORTS/reporte_usuarios.pdf"));
             PdfDocument pdfDoc = new PdfDocument(writer);
             Document document = new Document(pdfDoc);
 
             // Título del reporte
-            Paragraph title = new Paragraph("REPORTE DE TODOS LOS USUARIOS DE NUESTRA APLICACIÓN")
+            Paragraph title = new Paragraph("REPORTE DE USUARIOS, ACTIVIDADES Y CALORIAS")
                     .setFontSize(16);
             title.setTextAlignment(TextAlignment.CENTER);
             document.add(title);
             document.add(new Paragraph("\n"));  // Salto de línea
 
-            // Crear la tabla con 2 columnas
-            Table table = new Table(2); // 2 columnas
+            // Crear la tabla con 3 columnas
+            Table table = new Table(3); // 3 columnas
 
             // Establecer los anchos de las columnas en porcentaje
             table.setWidth(UnitValue.createPercentValue(100)); // Hace que la tabla ocupe el 100% del ancho disponible
 
             // Agregar los encabezados de la tabla
-            table.addCell(new Cell().add(new Paragraph("NOMBRE")).setTextAlignment(TextAlignment.CENTER));
-            table.addCell(new Cell().add(new Paragraph("PWD")).setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(new Paragraph("USUARIO")).setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(new Paragraph("ACTIVIDAD")).setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(new Paragraph("CALORIAS")).setTextAlignment(TextAlignment.CENTER));
 
             // Obtener los datos de la base de datos
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT NOMBRE, PWD FROM USER");
+            ResultSet rs = stmt.executeQuery("SELECT nombre, actividad, calorias FROM USUARIO");
 
             // Agregar los datos de la base de datos a la tabla
             while (rs.next()) {
-                table.addCell(new Cell().add(new Paragraph(rs.getString("NOMBRE"))).setTextAlignment(TextAlignment.LEFT));
-                table.addCell(new Cell().add(new Paragraph(rs.getString("PWD"))).setTextAlignment(TextAlignment.LEFT));
+                table.addCell(new Cell().add(new Paragraph(rs.getString("nombre"))).setTextAlignment(TextAlignment.LEFT));
+                table.addCell(new Cell().add(new Paragraph(rs.getString("actividad"))).setTextAlignment(TextAlignment.LEFT));
+                table.addCell(new Cell().add(new Paragraph(rs.getString("calorias"))).setTextAlignment(TextAlignment.LEFT));
             }
 
             // Añadir la tabla al documento PDF
@@ -63,18 +66,23 @@ public class ReportGenerating {
             document.close();
 
             System.out.println("Reporte generado exitosamente.");
+            //mostramos la alerta
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Reporte generado");
+            alert.setHeaderText("Reporte generado exitosamente.");
+            alert.setContentText("El reporte se ha generado con éxito en la carpeta REPORTS.");
+            alert.showAndWait();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
+
     // Método para establecer la conexión con la base de datos SQLite
     public static Connection connect() {
         try {
             // Asegúrate de tener el driver JDBC de SQLite en tu proyecto maven
-          //  String url = "jdbc:sqlite:data/bbdd_prueba.db";
-            Connection conn = DriverManager.getConnection(Constantes.RUTA_BBDD.getDescripcion());
+            Connection conn = DriverManager.getConnection(Constantes.RUTA_BBDD_CALORIAS.getDescripcion());
             return conn;
         } catch (Exception e) {
             e.printStackTrace();
